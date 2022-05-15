4cfec0324193676d5f0e41bb14e1adf0971704cf
Adam <adamp@kpapro.com>
2022-05-15 14:36:00 -0400

    feat(app):
    - Add Angular
    - Use HTTPS

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent implements OnInit {
+  title = 'Hey Purdy, Angular again!!??';
+  users: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: res => this.users = res,
+      error: err => console.error(err)
+    })
+   }
+}
